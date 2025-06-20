213c629ad813ac205ddbadea50f7f889f6048e45
LinJieXiao-XLJ <1546674204@qq.com>
2025-06-20 17:54:21 +0800

    Update the configuration of CMakeLists in the test directory

--- run.sh
+++ run.sh
@@ -1,55 +1,41 @@
 #!/bin/bash
 
-function tree_test() {
+function example() {
     # 检查文件是否存在并且是可执行的
-    if [ -x "./build/test/test/tree/$filename" ]; then
+    if [ -x "./build/example/$filename" ]; then
         echo "/******* Start test ********/"
-        cd ./build/test/tree
+        cd ./build/example
         # 执行文件
-        ./main
+        ./"$filename"
     else
         # 文件不存在或不可执行，打印错误信息
         echo "!!!错误:build/test/tree 目录下 $filename 文件不存在或不是可执行文件。"
     fi
 }
 
-function table_test() {
+function test() {
     # 检查文件是否存在并且是可执行的
-    if [ -x "./build/test/table/$filename" ]; then
+    if [ -x "./build/test/$filename" ]; then
         echo "/******* Start test ********/"
-        cd ./build/test/table
+        cd ./build/test
         # 执行文件
-        ./main
+        ./"$filename" --gtest_output="json:cpp_session_test_report.json"
     else
         # 文件不存在或不可执行，打印错误信息
-        echo "!!!错误:build/test/table 目录下 $filename 文件不存在或不是可执行文件。"
+        echo "!!!错误:build/test 目录下 $filename 文件不存在或不是可执行文件。"
     fi
 }
+
+# 默认测试模式
+mode="test"
+filename="main"
 
 # 根据输入的模式执行对应的测试
 while getopts ":m:" opt ; do
     # 根据选项执行不同的操作
     case $opt in
         m)
-            case $OPTARG in
-                tree)
-                    tree_test
-                    exit 0
-                    ;;
-                table)
-                    table_test
-                    exit 0
-                    ;;
-                all)
-                    tree_test
-                    table_test
-                    exit 0
-                    ;;
-                *)
-                    echo "输入的模型无效，请输入 tree 或 table (区分大小写)"
-                    exit 1
-                    ;;
-            esac
+            mode=$OPTARG
             ;;
         *)
             echo "无效的选项: -$OPTARG" >&2
@@ -58,19 +44,18 @@ while getopts ":m:" opt ; do
     esac
 done
 
-# 提示用户输入测试的模型
-echo "请输入测试的模型(tree/table):"
-read mode
-filename="main"
+# 根据模式执行对应的函数
 case $mode in
-    tree)
-        tree_test
+    example)
+        example
+        exit 0
         ;;
-    table)
-        table_test
+    test)
+        test
+        exit 0
         ;;
     *)
-        echo "输入的模型无效，请输入 tree 或 table(区分大小写)"
+        echo "输入的模型无效，请输入 example 或 test (区分大小写)"
         exit 1
         ;;
 esac
